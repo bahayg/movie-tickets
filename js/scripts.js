0c6c595d79cdf5a1a72fb5508c6f34f36587f80f
Student <example@example.com>
2019-08-29 12:00:51 -0700

    Change the inputs to checked, fix the calcPrice function by adding paranthesis after functions, and change show to text.

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,55 @@
 // Business Logic for AddressBook ---------
-
-// Price.prototype.calculateTicket = function() {
-//   for (var i=0; i<this.tickets.length; i++) {
-//     if (this.tickets[i] === ("godfather") {
-//         return this.tickets -5
-//         return this.tickets[i];
-//       }
-//     }
-//   };
-//   return false;
-// }
-
-// Business Logic for Tickets ---------
-function Ticket(movieName, matinee, age) {
+// Ticket constructor
+function Ticket(movieName, matinee, age, price) {
   this.movieName = movieName,
   this.matinee = matinee,
-  this.age = age
+  this.age = age,
+  this.price = price
 }
+
 Ticket.prototype.nameDiscount = function() {
   if (this.movieName === "godfather") {
-    return this.nameDiscount === -5
+    return -5;
   } else {
-    return this.nameDiscount === 0
+    return 0;
   }
 }
+
 Ticket.prototype.matineeDiscount = function() {
   if (this.matinee === "morning") {
-    return this.matineeDiscount === -5
+    return -5;
   } else {
-    return this.matineeDiscount === 0
+    return 0;
   }
 }
+
 Ticket.prototype.ageDiscount = function () {
   if (this.age === "children" || this.age === "senior") {
-    return this.ageDiscount === -5
+    return -5;
   } else {
-    return this.ageDiscount === 0
+    return 0;
   }
 }
-function ticketPrice () {
-  this.currentPrice === 30   
+
+Ticket.prototype.calcPrice = function () {
+  return this.price + this.nameDiscount() + this.matineeDiscount() + this.ageDiscount();
 }
 
-Ticket.prototype.ticketPrice = function () {
-  return this.currentPrice + this.nameDiscount + this.matineeDiscount + this.ageDiscount
-}
-
-
-// User Interface Logic ---------
+// User Interface Logic
 $(document).ready(function() {
-  //attachContactListeners();
-  $(".form-group#new-ticket").submit(function(event) {
+  $("form#movies").submit(function(event) {
     event.preventDefault();
-    var inputtedMovieName = $("input#new-movie-name").val();
-    var inputtedMatinee = $("input#new-matinee").val();
-    var inputtedAge = $("input#new-age").val();
+    var inputtedMovieName = $("input:checkbox[name=movie]:checked").val();
+    var inputtedMatinee = $("input:checkbox[name=matinee]:checked").val();
+    var inputtedAge = $("input:checkbox[name=age]:checked").val();
+
+    var newTicket = new Ticket (inputtedMovieName, inputtedMatinee, inputtedAge, 30);
+    //console.log(newTicket.calcPrice());
+    $("#price").show();
+    $("#price").text("Your ticket price is " + newTicket.calcPrice());
 
     $("input#new-movie-name").val("");
     $("input#new-matinee").val("");
     $("input#new-age").val("");
-
-    // var newTicket = new Ticket (inputtedMovieNAme, inputtedMatinee, inputtedAge);
-    // price.addTicket(newContact);
-    // displayContactDetails(addressBook);
   });
 });
